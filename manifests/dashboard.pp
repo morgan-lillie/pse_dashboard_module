5cbf4bc6a9af9c3245628b796ccf3fe259df8b35
SE Demo User <morgan.lillie@puppet.com>
2021-06-10 14:41:41 +0100

    Updated metadata for dependencies and added ruby plugin

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -1,15 +1,22 @@
+#
 class pse_dashboard_module::dashboard{
 
-
-file { '/home/centos/dash':
-    ensure => 'directory',
-
-}
-
-vcsrepo {'/home/centos/dash/smashing_dash_pse':
-  ensure    => present,
-  provider  => git,
-  source    => 'https://github.com/chrislorro/smashing_dash_pse.git',
+  include rbenv
+  rbenv::plugin { 'rbenv/ruby-build': }
+  rbenv::build { '3.0.1':
+    global => true,
   }
+
+
+  file { '/home/centos/dash':
+    ensure  => 'directory',
+  }
+
+  vcsrepo {'/home/centos/dash/smashing_dash_pse':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/chrislorro/smashing_dash_pse.git',
+  }
+
 }
 
