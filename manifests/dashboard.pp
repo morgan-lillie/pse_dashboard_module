cee9d8978a899f49256fd79f64ab0010a6af5556
SE Demo User <morgan.lillie@puppet.com>
2021-06-10 13:50:10 +0100

    put address in quotes

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -1,10 +1,9 @@
-
 class pse_dashboard_module::dashboard{
 
 vcsrepo {~/dash/smashing_dash_pse
   ensure    => present,
   provider  => git,
-  source    => https://github.com/chrislorro/smashing_dash_pse.git
+  source    => 'https://github.com/chrislorro/smashing_dash_pse.git',
   }
 }
 
