e8a8823cf742983eb56fc84c4d48598e58f7fb2d
SE Demo User <morgan.lillie@puppet.com>
2021-06-10 13:42:50 +0100

    Fixed class

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -1,8 +1,9 @@
-class pase_dashboard_module::dashboard (
+class pase_dashboard_module::dashboard{
 
 vcsrepo {~/dash/smashing_dash_pse
   ensure    => present,
   provider  => git,
   source    => https://github.com/chrislorro/smashing_dash_pse.git
   }
-)
+}
+
