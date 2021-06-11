8045de4d04d82b2fadc4bfa82e79675a1b1d0018
SE Demo User <morgan.lillie@puppet.com>
2021-06-11 10:42:29 +0100

    fixed path... again

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -21,7 +21,7 @@ class pse_dashboard_module::dashboard{
 
   exec { 'install_bundler_gems':
     path      => '/usr/local/rbenv/shims',
-    command   => '$bundle install',
+    command   => 'bundle install',
     subscribe =>  Vcsrepo['/home/centos/dash/smashing_dash_pse'],
     cwd       => '/home/centos/dash/smashing_dash_pse',
   }
